e4218be9a0500a5859185c14c485439563a4593e
Riccardo Quercetti <riccardo.quercetti.93@gmail.com>
2013-12-04 23:06:08 +0100

    Versione 2.0

--- ComfortFirmwareArduino/ComfortFirmwareArduino.ino
+++ ComfortFirmwareArduino/ComfortFirmwareArduino.ino
@@ -5,30 +5,40 @@
  * ANA3 = LUCE = PIN9
  * DIG19 = ERRORE
  */
- 
-//Impostazioni
-#define sensorsBufferLength 60
-#define sensorsReadsDelay 50
-#define sensorsFiltrationDelay 5
-#define errorPin 3
 
 #include <SPI.h>
 #include <Ethernet.h>
+#include <SD.h>
 
-long lastUDPSend;
-byte mac[] = {0x90, 0xA2, 0xDA, 0x00, 0x0C, 0xD9};//Indirizzo di mac della eth schield
+//Impostazioni
+#define errorPin 19
+#define sensorsReadsDelay 10
+#define noiseAverageBufferLength 100
+
+// Informazioni di configurazione della NIC
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+// Creo l'istanza del server HTTP
+EthernetServer server(80);
+EthernetClient client;
+File requestedFile;
+boolean isEthernetConnected = false;
+boolean isSDAvailable = false;
 int sensors[5];//Valori dei sensori
+int noiseAverageBuffer[noiseAverageBufferLength];
+int noiseAverageBufferIndex = 0;
+long noiseAverage;
 byte sensorsLeds[] = {2, 7, 8, 9};//Pin dei led di allarme dei sensori
 float sensorsComfort[4];//Livello di comfort dei singoli parametri
 int weight[4] = {361, 252, 205, 205};//Importanza dei parametri nella formula del comfort index
 int sensorsRanges[] = {200, 1020, 200, 1020, 200, 1020, 200, 1020};//Range di valori in cui si ha comfort
-int sensorsBuffer[4][sensorsBufferLength];
-int sensorsBufferOrdered[4][sensorsBufferLength];
-int sensorsBufferIndex = 0;
-int sensorsReadsIndex = 0;
 long lastSensorsRead = millis();
+IPAddress ip (192, 168, 1, 6);
 
-void setup(){
+void setup() {
+  //Inizializzo il buffer per la media del suono
+  for(int a = 0; a < noiseAverageBufferLength; a++){
+    noiseAverageBuffer[a] = 0;
+  }
   //Inizializazione dei led di allarme dei sensori
   for(int a = 0; a < 4; a++){
     pinMode(sensorsLeds[a], OUTPUT);
@@ -36,83 +46,49 @@ void setup(){
   }
   pinMode(errorPin, OUTPUT);
   digitalWrite(errorPin, LOW);
-  //Inizializzo apparato ethernet e il server UDP
-  /*if(Ethernet.begin(mac) == 0){
-    digitalWrite(19, HIGH);
-  } else {
-    // Inizializzato con successo, inizializzo il web server
-    
+  // Abilito gli SS per usare sia la NIC che l'SD reader
+  pinMode(10, OUTPUT);
+  pinMode(4, OUTPUT);
+  // Inizializzo la parte di rete
+  Ethernet.begin(mac, ip);
+  isEthernetConnected = true;
+  /*isEthernetConnected = Ethernet.begin(mac);
+  if (!isEthernetConnected) {
+    digitalWrite(errorPin, HIGH);
   }*/
-  pinMode(3, OUTPUT);
+  server.begin();
+  //Inizializzo la parte di memorizzazione
+  isSDAvailable = SD.begin(4);
+  if (!isSDAvailable) {
+    digitalWrite(errorPin, HIGH);
+  }
 }
-void loop(){
-  delay(1000);
-  digitalWrite(errorPin, HIGH);
-  delay(1000);
-  digitalWrite(errorPin, LOW);
+
+void loop() {
   /**** INIZIO ACQUISIZIONE DATI ****/
   if(millis() - lastSensorsRead >= sensorsReadsDelay){
     for(int a = 0; a < 4; a++){
-      sensorsBuffer[a][sensorsBufferIndex] = analogRead(a);
+      sensors[a] = analogRead(a);
     }
-    if(++sensorsBufferIndex >= sensorsBufferLength){
-      sensorsBufferIndex = 0;
-    }
-    sensorsReadsIndex++;
     lastSensorsRead = millis();
+    if(sensors[2] >= 512){
+      sensors[2] -= 512;
+    } else {
+      sensors[2] = 0;
+    }
+    noiseAverageBuffer[noiseAverageBufferIndex] = sensors[2];
+    if(noiseAverageBufferIndex + 1 < noiseAverageBufferLength){
+      noiseAverageBufferIndex ++;
+    } else {
+      noiseAverageBufferIndex = 0;
+    }
+    noiseAverage = 0;
+    for(int a = 0; a < noiseAverageBufferLength; a++){
+      noiseAverage += noiseAverageBuffer[a];
+    }
+    sensors[2] = noiseAverage / noiseAverageBufferLength;
   }
   /**** FINE ACQUISIZIONE ****/
-  /**** INIZIO FILTRO DATI ****/
-  if(sensorsReadsIndex >= sensorsFiltrationDelay){
-    for(int a = 0; a < 4; a++){
-      for(int b = 0; b < sensorsBufferLength; b++){
-        sensorsBufferOrdered[a][b] = sensorsBuffer[a][b];
-      }
-    }
-    int bubbleSortSwap = 0;
-    for(int a = 0; a < 4; a++){
-      //Ordino il vettore delle letture
-      int n = sensorsBufferLength;
-      int p = sensorsBufferLength;
-      int flag = 0;
-      do{
-        flag = 0;
-        for(int b = 0; b < n - 1; b++){
-          if(sensorsBufferOrdered[a][b] < sensorsBufferOrdered[a][b + 1]){
-            bubbleSortSwap = sensorsBufferOrdered[a][b];
-            sensorsBufferOrdered[a][b] = sensorsBufferOrdered[a][b + 1];
-            sensorsBufferOrdered[a][b + 1] = bubbleSortSwap;
-            flag = 1;
-            p = b + 1;
-          }
-        }
-        n = p;
-      }while(flag == 1);
-      //Ricavo il valore più frequente
-      int modeValue = 0;
-      int modeValue2 = sensorsBufferOrdered[a][0];
-      int modeTimes = 0;
-      int modeTimes2 = 1;
-      for(int b = 1; b < sensorsBufferLength + 1; b++){
-        if(sensorsBufferOrdered[a][b] == modeValue2 && b < sensorsBufferLength){
-          modeTimes2++;
-        } else {
-          if(modeTimes < modeTimes2){
-            modeValue = modeValue2;
-            modeTimes = modeTimes2;
-          }
-          if(b < sensorsBufferLength){
-            modeValue2 = sensorsBufferOrdered[a][b];
-            modeTimes2 = 1;
-          }
-        }
-      }
-      // Prendo il valore moda e lo considero come il valore acquisito dai sensori
-      sensors[a] = modeValue;
-    }
-    sensorsReadsIndex = 0;
-  }
-  /**** FINE FILTRO DATI ****/
   /**** INIZIO GESTIONE LED ****/
   for(int a = 0; a < 8; a +=2){
     //Se il valore è minore del valore ideale e minore del limite minimo, accendo il led relativo
@@ -143,4 +119,90 @@ void loop(){
   // Piloto lancetta in base al comfort index
   analogWrite(6, map(sensors[4], 0, 1023, 0, 255));
   /**** FINE GESTIONE LED ****/
+  /**** INIZIO GESTIONE RICHIESTE INFORMAZIONI ****/
+  // Se si è connessi
+  if (isEthernetConnected) {
+    // Controllo se ci sono clients in attesa di dati
+    client = server.available();
+    if (client) {
+      // Se l'handshake è andato a buon fine
+      if (client.connected()) {
+        // Memorizzo il tipo di richiesta (keyword) e l'eventuale file richiesto
+        char requestKeyword [5];
+        char requestedFileName [15];
+        scanHTTPRequest(requestKeyword, requestedFileName);
+        client.flush();
+        // Rispondo alla richiesta passando il file
+        // Se è richiesto il file root, cambio il nome file da / a /client.htm
+        if (strcmp("/", requestedFileName) == 0) {
+          strcpy(requestedFileName, "/client.htm");
+        }
+        // Controllo che il file esista o se è un file dinamico da generare
+        if (SD.exists(requestedFileName) || strcmp(requestedFileName, "/sensors.xml") == 0) {
+          // Creo l'header della risposta, personalizzandolo in base al tipo di dato passato
+          client.print("HTTP/1.1 200 OK\nContent-Type: ");
+          char requestedFileExtension [3];
+          getFileNameExtension(requestedFileName, requestedFileExtension);
+          if(strcmp(requestedFileExtension, "css") == 0){
+            client.print("text/css");
+          } else if (strcmp(requestedFileExtension, "xml") == 0) {
+            client.print("text/xml");
+          } else if (strcmp(requestedFileExtension, "js") == 0) {
+            client.print("text/javascript");
+          } else {
+            client.print("text/html");
+          }
+          client.println("\nConnection: close\n");
+          if (strcmp(requestedFileName, "/sensors.xml") == 0) {
+            // Se il file richiesto è sensors.xml, allora invio un XML con i dati dei sensori
+            client.print("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<sensors>\n\t<sensor name=\"CO2\">");
+            client.print(sensors[0]);
+            client.print("</sensor>\n\t<sensor name=\"temperature\">");
+            client.print(sensors[1]);
+            client.print("</sensor>\n\t<sensor name=\"noise\">");
+            client.print(sensors[2]);
+            client.print("</sensor>\n\t<sensor name=\"brightness\">");
+            client.print(sensors[3]);
+            client.print("</sensor>\n</sensors>");
+          } else {
+            // E' un file esistente nell'SD, lo apro e lo invio
+            requestedFile = SD.open(requestedFileName);
+            char c;
+            while ((c = requestedFile.read()) > 0) {
+                client.print(c);
+            }
+            requestedFile.close();
+          }
+        } else {
+          // Il file non esiste, restituisco con un errore 404
+          client.println("HTTP/1.1 404 Not Found\nContent-Type: text/html\nConnection: close\n");
+          client.print("<!DOCTYPE HTML>\n<html><h1>Errore 404</h1><h2>Il file ");
+          client.print(requestedFileName);
+          client.println(" non esiste.</h2></html>");
+        }
+      }
+      // Lascio che il browser riceva tutti i dati
+      delay(1);
+      // Chiudo la connessione
+      client.stop();
+    }
+  }
+  /**** FINE GESTIONE RICHIESTE INFORMAZIONI ****/
+}
+
+// Restituisce la keyword e il nome dell'eventuale file richiesto
+void scanHTTPRequest (char *requestKeyword, char *requestedFileName) {
+  int requestKeywordLength = client.readBytesUntil(' ', requestKeyword, 4);
+  requestKeyword[requestKeywordLength] = '\0';
+  int requestedFileNameLength = client.readBytesUntil(' ', requestedFileName, 14);
+  requestedFileName[requestedFileNameLength] = '\0';
+  return;
+}
+
+// Restituisce l'estenzione del nome file passatogli
+void getFileNameExtension (char *fileName, char *extension) {
+  char *fileNameDot = strchr(fileName, '.');
+  *fileNameDot = ' ';
+  sscanf(fileName, "%*s %s", extension);
+  *fileNameDot = '.';
 }
